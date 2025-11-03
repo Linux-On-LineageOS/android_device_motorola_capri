ff44b1fe5097a3bb11b1f7500df977d8a2ce1bfd
Archie Meng <archiemeng@protonmail.com>
2025-11-03 20:28:51 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -94,3 +94,4 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/capri/capri-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
